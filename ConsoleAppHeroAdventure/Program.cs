f084fc6183ef1b81a5e2d7f0bb4288ab7711467d
itpark <a.m.smyslov@yandex.ru>
2023-01-04 11:40:23 +0300

    done proc and func

--- ConsoleAppHeroAdventure/Program.cs
+++ ConsoleAppHeroAdventure/Program.cs
@@ -8,37 +8,29 @@
 //3) передвижение героя по полю
 //4) вход героя в портал и переход к шагу 1
 
-
-//bhjksadbfjkhsdbfjhsbjdhfbsjhdbfjhsbdfbj
-//sdjkfbkjsdbfksjdbfdskj
 using ConsoleAppHeroAdventure;
 
-Random random = new Random();
-
-int currentLevel = 1;
-int rows, cols;
-Cell[,] field;
-
-int iHero, jHero;
-
-bool heroInAdventure;
-
-int currentWallPercent = (int)Constants.WallPercent;
-
-// int[,] dogs = new int[,]
-// {
-//     { 0, 0 },
-//     { 0, 0 }
-// };
-//
-// int iDog, jDog;
-
-while (true)
+int InitRows()
 {
-    rows = random.Next((int)Constants.MinRows, (int)Constants.MaxRows + 1);
-    cols = random.Next((int)Constants.MinCols, (int)Constants.MaxCols + 1);
+    Random random = new Random();
+    return random.Next((int)Constants.MinRows, (int)Constants.MaxRows + 1);
+}
 
-    field = new Cell[rows, cols];
+int InitCols()
+{
+    Random random = new Random();
+    return random.Next((int)Constants.MinCols, (int)Constants.MaxCols + 1);
+}
+
+Cell[,] CreateField(int rows, int cols)
+{
+    return new Cell[rows, cols];
+}
+
+void InitField(Cell[,] field)
+{
+    int rows = field.GetLength(0);
+    int cols = field.GetLength(1);
 
     for (int i = 0; i < rows; i++)
     {
@@ -59,9 +51,23 @@ while (true)
         field[0, j] = Cell.Bound;
         field[rows - 1, j] = Cell.Bound;
     }
+}
 
-    iHero = (int)Constants.StartIHero;
-    jHero = (int)Constants.StartJHero;
+int InitIHero()
+{
+    return (int)Constants.StartIHero;
+}
+
+int InitJHero()
+{
+    return (int)Constants.StartJHero;
+}
+
+void SetPortal(Cell[,] field, int iHero, int jHero)
+{
+    Random random = new Random();
+    int rows = field.GetLength(0);
+    int cols = field.GetLength(1);
 
     int iPortal, jPortal;
     do
@@ -71,6 +77,13 @@ while (true)
     } while (iPortal == iHero && jPortal == jHero);
 
     field[iPortal, jPortal] = Cell.Portal;
+}
+
+void SetWalls(Cell[,] field, int currentWallPercent, int iHero, int jHero)
+{
+    Random random = new Random();
+    int rows = field.GetLength(0);
+    int cols = field.GetLength(1);
 
     int countWalls = (int)((rows - 2) * (cols - 2) * currentWallPercent / 100.0);
     for (int i = 0; i < countWalls; i++)
@@ -86,95 +99,148 @@ while (true)
 
         field[iWall, jWall] = Cell.Wall;
     }
+}
 
-    heroInAdventure = true;
-    while (heroInAdventure)
+void ResetConsole()
+{
+    Console.Clear();
+    Console.ResetColor();
+}
+
+void PrintGameInfo(int level, int wallPercent)
+{
+    Console.WriteLine($"Level = {level}, Wall Percent = {wallPercent}");
+}
+
+void PrintField(Cell[,] field, int iHero, int jHero)
+{
+    int rows = field.GetLength(0);
+    int cols = field.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
     {
-        Console.Clear();
-
-        Console.ResetColor();
-
-        Console.WriteLine($"Current Level = {currentLevel}");
-        for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
         {
-            for (int j = 0; j < cols; j++)
+            if (i == iHero && j == jHero)
             {
-                if (i == iHero && j == jHero)
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write((char)Constants.HeroSkin);
+            }
+            else
+            {
+                switch (field[i, j])
                 {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write((char)Constants.HeroSkin);
+                    case Cell.Empty:
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    case Cell.Wall:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case Cell.Portal:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case Cell.Bound:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
                 }
-                else
-                {
-                    switch (field[i, j])
-                    {
-                        case Cell.Empty:
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            break;
-                        case Cell.Wall:
-                            Console.ForegroundColor = ConsoleColor.DarkRed;
-                            break;
-                        case Cell.Portal:
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            break;
-                        case Cell.Bound:
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            break;
-                    }
 
-                    Console.Write((char)field[i, j]);
-                }
+                Console.Write((char)field[i, j]);
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void MoveHero(Cell[,] field, ref int iHero, ref int jHero, ref bool heroInAdventure)
+{
+    ConsoleKey key = Console.ReadKey(false).Key;
+    switch (key)
+    {
+        case ConsoleKey.A:
+            if (field[iHero, jHero - 1] == Cell.Empty || field[iHero, jHero - 1] == Cell.Portal)
+            {
+                jHero--;
             }
 
-            Console.WriteLine();
-        }
+            break;
 
-        ConsoleKey key = Console.ReadKey(false).Key;
-        switch (key)
-        {
-            case ConsoleKey.A:
-                if (field[iHero, jHero - 1] == Cell.Empty || field[iHero, jHero - 1] == Cell.Portal)
-                {
-                    jHero--;
-                }
+        case ConsoleKey.W:
+            if (field[iHero - 1, jHero] == Cell.Empty || field[iHero - 1, jHero] == Cell.Portal)
+            {
+                iHero--;
+            }
 
-                break;
+            break;
 
-            case ConsoleKey.W:
-                if (field[iHero - 1, jHero] == Cell.Empty || field[iHero - 1, jHero] == Cell.Portal)
-                {
-                    iHero--;
-                }
+        case ConsoleKey.D:
+            if (field[iHero, jHero + 1] == Cell.Empty || field[iHero, jHero + 1] == Cell.Portal)
+            {
+                jHero++;
+            }
 
-                break;
+            break;
 
-            case ConsoleKey.D:
-                if (field[iHero, jHero + 1] == Cell.Empty || field[iHero, jHero + 1] == Cell.Portal)
-                {
-                    jHero++;
-                }
+        case ConsoleKey.S:
+            if (field[iHero + 1, jHero] == Cell.Empty || field[iHero + 1, jHero] == Cell.Portal)
+            {
+                iHero++;
+            }
 
-                break;
+            break;
 
-            case ConsoleKey.S:
-                if (field[iHero + 1, jHero] == Cell.Empty || field[iHero + 1, jHero] == Cell.Portal)
-                {
-                    iHero++;
-                }
-
-                break;
-
-            case ConsoleKey.R:
-                heroInAdventure = false;
-                break;
-        }
-
-        if (field[iHero, jHero] == Cell.Portal)
-        {
-            currentLevel++;
-            currentWallPercent += 5;
+        case ConsoleKey.R:
             heroInAdventure = false;
+            break;
+    }
+}
+
+bool IsHeroInPortal(Cell[,] field, int iHero, int jHero)
+{
+    return field[iHero, jHero] == Cell.Portal;
+}
+
+void GotoNextLevel(ref int currentLevel, ref int currentWallPercent, ref bool heroInAdventure)
+{
+    currentLevel++;
+    currentWallPercent += 1;
+    heroInAdventure = false;
+}
+
+
+//-------
+
+int currentLevel = 1;
+int currentWallPercent = (int)Constants.WallPercent;
+
+while (true)
+{
+    int rows = InitRows();
+    int cols = InitCols();
+
+    Cell[,] field = CreateField(rows, cols);
+    InitField(field);
+
+    int iHero = InitIHero();
+    int jHero = InitJHero();
+
+    SetPortal(field, iHero, jHero);
+    SetWalls(field, currentWallPercent, iHero, jHero);
+
+    bool heroInAdventure = true;
+
+    while (heroInAdventure)
+    {
+        ResetConsole();
+
+        PrintGameInfo(currentLevel, currentWallPercent);
+        PrintField(field, iHero, jHero);
+
+        MoveHero(field, ref iHero, ref jHero, ref heroInAdventure);
+
+        if (IsHeroInPortal(field, iHero, jHero))
+        {
+            GotoNextLevel(ref currentLevel, ref currentWallPercent, ref heroInAdventure);
         }
     }
-    
 }
